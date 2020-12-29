cf7a12cf393e2e9ef1f36572c70252006d737e85
prasanavadivelu <52127968+prasanavadivelu@users.noreply.github.com>
2020-12-29 19:24:59 +0530

    Create User.java

---
+++ src/main/java/com/review/entity/User.java
@@ -0,0 +1,25 @@
+package com.review.entity;
+
+public class User {
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String user;
+	
+	public String token;
+	
+}
